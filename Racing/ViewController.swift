d46e3412e6fbc994e8b46ee04f9f2d4474ed0e1f
Lieb12 <lieberman03@gmail.com>
2020-10-28 08:43:13 -0400

    Initial Commit

---
+++ Racing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Racing
+//
+//  Created by  on 10/28/20.
+//  Copyright © 2020 Owen Lieberman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
